dc637d7ebd98ea6fa4dec2216791ee071428f2f1
Mathieu Laversin <mathieulaversin @outlook.fr>
2023-09-05 14:02:34 +0200

    Updates

--- static/chunks/webpack.js
+++ static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62b8094462437cda9910"; }
+/******/ 		__webpack_require__.h = function() { return "29b5dee885a46499688b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
